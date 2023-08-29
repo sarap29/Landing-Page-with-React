78c77b2f2cd01e95dcf533263e27e099817fba14
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-08-29 15:50:02 +0000

    se crean archivos jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,23 @@
 import React from "react";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div>
+          <Navbar />
+		  <Jumbotron />
+		  <div className= " container d-flex justify-content-between">
+			<Card />
+			<Card />
+			<Card />
+			<Card />
+		  </div>
+        </div>
 	);
 };
 
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+const Navbar = () => {
+return (
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Start React</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse d-flex justify-content-end">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">About</a>
+        <a className="nav-link" href="#">Services</a>
+        <a className="nav-link" href="#">Contact</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+);
+};
+
+export default Navbar;
+
