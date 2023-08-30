e8e433d3c287c6614b118e7c7c056dff0ceb7217
SaraPe <135864591+sarap29@users.noreply.github.com>
2023-08-30 21:09:43 +0000

    Arreglo el navbar, la versión responsive

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -19,3 +19,7 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
+
--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,25 +3,25 @@ import React from "react";
 
 const Navbar = () => {
 return (
-<nav class="navbar navbar-expand-lg bg-dark">
-  <div class="container">
-    <a class="navbar-brand text-white" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg bg-dark">
+  <div className="container">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-collapse collapse show d-flex justify-content-end" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
